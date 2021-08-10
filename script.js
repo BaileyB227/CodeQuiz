d5e5df88fa1f7839bd165fb93c7686fcc9683d86
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-09 23:32:28 -0500

    added clearscore, cleartime, playagain, and local storage info

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-let startBtn = document.querySelector("#start")
-let timeLeft =  document.querySelector("#time")
-let falseBtn = document.querySelector("#falseBtn")
-let trueBtn = document.querySelector("#trueBtn")
-let questionEl = document.querySelector("#question")
-let highscoreEl = document.querySelector("#highscore")
-let initialsEl = document.querySelector("#initials")
-let formInitials = document.querySelector("#formInitials")
-let highscoreName = document.getElementById("#highscoreNames")
-let highscoreScore = document.getElementById("#highscoreScore")
-let highscoreBtn = document.createElement("button")
-let score = 0
-let timer = 60
-let currentQuestion = 0
+let startBtn = document.querySelector("#start");
+let timeLeft =  document.querySelector("#time");
+let falseBtn = document.querySelector("#falseBtn");
+let trueBtn = document.querySelector("#trueBtn");
+let questionEl = document.querySelector("#question");
+let highscoreEl = document.querySelector("#highscore");
+let initialsEl = document.querySelector("#initials");
+let formInitials = document.querySelector("#formInitials");
+let scoreListEl = document.querySelector("#highscoreList");
+let inputInitials = document.querySelector("#initialInput");
+let addScore = document.querySelector("#addHighscore");
+let playAgainBtn = document.querySelector("#playAgain");
+let score = 0;
+let timer = 60;
+let currentQuestion = 0;
+let scoreList = [];
 
 trueBtn.disabled = true;
 falseBtn.disabled = true;
 initialsEl.style.display = "none";
+displayScores();
 
 
 let quizQuestions = ["Are apples red",
@@ -30,8 +33,7 @@ let quizQuestions = ["Are apples red",
 "People have 4 eyes"]
 
 function countdown() {
-
-    var timerInterval = setInterval(function() {
+    let timerInterval = setInterval(function() {
         timer--;
         timeLeft.textContent = timer;
 
@@ -57,17 +59,68 @@ function nextQuestion(){
 function endGame(){
     trueBtn.disabled = true;
     falseBtn.disabled = true;
+    startBtn.disabled = false;
+    addScore.disabled = false;
     initialsEl.style.display = "block";
     questionEl.textContent = "Game over! Score: " + score
-    
-    highscoreBtn.innerHTML = "Submit"
-
-    formInitials.appendChild(highscoreBtn)
 }
+
+function clearTime(){
+    timer = 60;
+    startGame();
+    countdown();
+}
+
+function clearScore(){
+    score = 0;
+}
+
+function storeScore(){
+    localStorage.setItem("scoreList", JSON.stringify(scoreList))
+}
+
+function displayScores(){
+    let allHighscores = JSON.parse(localStorage.getItem("scoreList"));
+
+    if (allHighscores !== null) {
+        scoreList = allHighscores
+    }
+}
+
+addScore.addEventListener("click", function(event){
+    event.preventDefault();
+
+    let init = inputInitials.value.toUpperCase();
+    scoreList.push({ initials: init, score: score});
+
+    scoreList = scoreList.sort((a, b) =>{
+        if (a.score < b.score) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    scoreListEl.innerHTML="";
+    for (let i = 0; i < scoreList.length;i++){
+        let li = document.createElement("li")
+        li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
+        scoreListEl.appendChild(li);
+    }
+
+    storeScore();
+    displayScores();
+    addScore.disabled = true;
+})
 
 startBtn.addEventListener("click", function(){
     countdown();
     startGame();
+})
+
+playAgainBtn.addEventListener("click", function(){
+    clearTime();
+    clearScore();
 })
 
 trueBtn.addEventListener("click", function(){
@@ -109,29 +162,3 @@ falseBtn.addEventListener("click", function(){
         endGame();
     }
 })
-
-highscoreBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    let savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
-    let currentUser = initialInput.value.trim();
-    let currentHighscore = {
-        name: currentUser, 
-        score: score
-    };
-
-    savedHighscores.push(currentHighscore);
-    localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
-    generateHighscores();
-})
-
-function generateHighscores(){
-    let highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
-    for (i=0; i<highscores.length; i++){
-        let nameSpan = document.createElement("li");
-        let scoreSpan = document.createElement("li");
-        nameSpan.textContent = highscores[i].name;
-        scoreSpan.textContent = highscores[i].score;
-        highscoreName.appendChild(nameSpan);
-        highscoreScore.appendChild(scorespan);
-    }
-}
